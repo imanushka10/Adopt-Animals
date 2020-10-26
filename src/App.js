49dbaa43db8906c57c323ce876580ab4a6af9730
Anu Singh <anusingh1883656@gmail.com>
2020-10-26 21:52:52 +0530

    Adopt-animals

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext"
+
+
+
+
+const App = () => {
+  const theme = useState("orange");
+  return (
+    <>
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams themeColor={theme}
+              path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ ThemeContext.Provider>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
