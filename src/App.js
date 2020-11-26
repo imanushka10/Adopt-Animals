1bae2f9a7c77467b01c7d4ee8a5997dfe1fb9fc2
Anu Singh <anusingh1883656@gmail.com>
2020-11-26 19:50:43 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,21 @@ import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
-import ThemeContext from "./ThemeContext"
-
+import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
-  const theme = useState("peru");
+  const theme = useState("darkblue");
   return (
-    <>
-      <ThemeContext.Provider value={theme}>
-        <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Router>
-            <SearchParams themeColor={theme}
-              path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </div>
-      </ ThemeContext.Provider>
-    </>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <NavBar />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
